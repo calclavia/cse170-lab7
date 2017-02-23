f1351336368a9127add3f3b883d28fec223cc268
Henry Mao <admin@calclavia.com>
2017-02-22 23:58:38 -0800

    Added GA for like button

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -2,12 +2,15 @@
 
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
-	initializePage();
+    initializePage();
 });
 
 /*
  * Function that is called when the document is ready.
  */
 function initializePage() {
-	// your code here
+    // your code here
+    $('.likeBtn').click(function() {
+        ga("send", "event", 'like', 'click');
+    });
 }
